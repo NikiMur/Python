9094b261b8ee4232314b7cd64ab7611b188bb94f
NikiMur <zik.9507@bk.ru>
2022-09-11 19:21:05 +0300

    2HomeWork

--- PythonHomeWork/2HomeWork/1Zadacha.py
+++ PythonHomeWork/2HomeWork/1Zadacha.py
@@ -1,15 +1,24 @@
 import random
 
-n = 5
+print()
+vibor = int(input("введите 1 если хотите рандомное число монеток или 2 если хотите указать их число "))
+if vibor == 1:
+    num = random.randint(1,10)
+    print(f"монеток {num} выпало  ")
+elif vibor == 2:
+    num = int(input("Введите число монеток: "))    
+else: 
+    print(f"ах так тогда получай столько монет сколько ввел {vibor}  ")
+    num = vibor
+   
 
-lst = [random.randint(0, 1) for i in range(n)]
-print(lst)
+count = 0
+print()
 
-if lst.count(1) > lst.count(0):
-    print(f"Минимальное число решек: {lst.count(0)}") 
-else:
-    print(f"Минимальное число гербов {lst.count(1)}")
+for i in range(num):
+    x = random.randint(0, 2)
+    if x == 0: count += 1
 
-
-print((f"Минимальное число решек: {lst.count(0)}", 
-f"Минимальное число гербов {lst.count(1)}")[lst.count(1) < lst.count(0)])     
+if count < num / 2: print(f"Необходимо перевернуть {count} монеток с гербом")
+elif count == num / 2: print(f"Необходимо перевернуть {count} монеток либо с гербом, либо с решкой")
+else: print(f"Необходимо перевернуть {num - count} монеток с решкой")
